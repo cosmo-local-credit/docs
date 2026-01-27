3061f4c9cfe32e356fd42f42d149b490242af3f1
Will Ruddick <willruddick@gmail.com>
2026-01-27 14:35:07 +0300

    feat: add landing page icons and update vocs.config for favicon and manifest

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,8 +1,29 @@
+import { createElement, Fragment } from 'react'
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
   title: 'Cosmo-Local Credit',
   description: 'cosmo-local credit',
+  iconUrl: '/icons/favicon.ico',
+  head: createElement(
+    Fragment,
+    null,
+    createElement('link', {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/icons/apple-touch-icon.png',
+    }),
+    createElement('link', {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '96x96',
+      href: '/icons/favicon-96x96.png',
+    }),
+    createElement('link', {
+      rel: 'manifest',
+      href: '/icons/site.webmanifest',
+    }),
+  ),
   theme: {
     accentColor: '#10b981',
   },
